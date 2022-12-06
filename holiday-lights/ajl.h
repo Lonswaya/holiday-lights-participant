5d004accd83b5a8c6c86b463bbf7143b74cf21ae
Andrew Lonsway <ajlonsway@git.sep.com>
2022-12-05 22:14:58 -0500

    Kinda vertical? Unintentional swoosh

--- holiday-lights/ajl.h
+++ holiday-lights/ajl.h
@@ -1,20 +1,33 @@
 #include <math.h>
 
+const double BOTTOM_LIGHT_COUNT = 31.3;
+const double TOP_LIGHT_COUNT = 5.0;
+const double LIGHT_SPEED = 1.0;
+const double LIGHT_SPREAD_MULT = 0.75;
 
+int lightRingCount(int startIndex)
+{
+  // lerp
+  return round((BOTTOM_LIGHT_COUNT + (startIndex / NUM_LEDS) * (TOP_LIGHT_COUNT - BOTTOM_LIGHT_COUNT)) * LIGHT_SPREAD_MULT);
+}
 
 void snowing(CRGB *leds)
 {
-  const double bottomLightCount = 31.3;
-  const double topLightCount = 5.0;
-  const double lightRate = 1.0;
   double theta = 0;
-  theta = (int(gLoopCounter * lightRate)) % 360;
+  theta = (int(gLoopCounter * LIGHT_SPEED)) % 360;
   double thetaPercent = theta / 360;
-  for (double z = 0; z <= 1.0; z = z + 0.03)
+  //double z = 0.5
+  for (int start = 0; start < NUM_LEDS; start = start + lightRingCount(start))
   {
-    int lightRingTotal = round(z * (bottomLightCount - topLightCount) + topLightCount);
-    int lightIndex = round((thetaPercent * lightRingTotal) + (z * NUM_LEDS));
-    leds[lightIndex] = CRGB::Blue;
+    int nextStep = lightRingCount(start);
+    if (start + round(nextStep * thetaPercent) < NUM_LEDS)
+    {
+      leds[start + int(round(double(nextStep) * thetaPercent))] = CRGB::Blue;
+    }
+    //if (start <= z < nextStep)
+    //{
+      
+    //}
   }
   fadeToBlackBy( leds, NUM_LEDS, 20);
 }
